6b3b6a5f4dd7ca1fc427f36def7845ba8ee5a04a
Fede Calendino <fede@calendino.com>
2022-09-12 11:36:19 +0200

    Migrate to pyinstaller --onedir

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
-poetry run pyinstaller --onefile --paths ./src/ src/main.py
+function build() {
+  echo "Generating $1 binary"
+  echo
 
-rm -rf ./build
-rm *.spec
+  poetry run pyinstaller --onedir --paths ./src/ "src/$1.py" 1> /dev/null
+
+  echo "Workspace cleanup"
+  echo
+
+  rm "./$1.spec"
+  rm -rf "./build/"
+
+  echo
+	echo "Finished generating bin file for $1"
+	echo
+}
+
+
+rm -rf "./dist/"
+
+build main
+
+echo "Finished"
+echo
---
+++ release.sh
@@ -0,0 +1,53 @@
+BUNDLEID=$(plutil -extract bundleid raw -o - ./info.plist)
+NAME=${BUNDLEID##*.}
+
+VERSION=${1:-$(poetry version --short)}
+
+FILENAME="$NAME.v$VERSION.alfredworkflow"
+
+poetry version $VERSION
+plutil -replace version -string $VERSION info.plist
+
+echo "NAME: $NAME"
+echo "BUNDLE ID: $BUNDLEID"
+echo "CURRENT VERSION: v$(poetry version --short)"
+echo "NEW VERSION: v$VERSION"
+echo
+
+echo "Building binaries..."
+echo
+
+./build.sh > /dev/null
+echo
+
+echo "Building release..."
+echo
+
+echo "Clean up non-exportable variables"
+echo
+
+# backup info.plist
+cp info.plist old.plist
+
+# find amount of variables to not export
+VARIABLES_DONT_EXPORT_AMOUNT=$(plutil -extract variablesdontexport raw -o - ./info.plist)
+
+for i in $(seq 0 $(expr "$VARIABLES_DONT_EXPORT_AMOUNT" - 1))
+do
+  VARIABLE=$(plutil -extract "variablesdontexport.$i" raw -o - ./info.plist)
+  plutil -replace "variables.$VARIABLE" -string "" ./info.plist
+
+  echo " * $VARIABLE: cleared"
+done
+
+mkdir releases 2> /dev/null
+zip "releases/$FILENAME" -r dist img *.png info.plist
+echo
+
+echo "Released $NAME v$VERSION"
+echo " * releases/$FILENAME"
+echo
+
+# restore original info.plist
+rm info.plist
+mv old.plist info.plist
