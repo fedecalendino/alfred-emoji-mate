3c32ce8e37af83c73ddcb00616d74a78c38937ed
Fede Calendino <fede@calendino.com>
2022-11-02 20:47:05 +0100

    Update for v1.8.1

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,25 +23,6 @@ echo
 
 echo "Building release..."
 echo
-
-echo "Clean up non-exportable variables"
-echo
-
-# backup info.plist
-cp info.plist old.plist
-
-# find amount of variables to not export
-VARIABLES_DONT_EXPORT_AMOUNT=$(plutil -extract variablesdontexport raw -o - ./info.plist)
-
-for i in $(seq 0 $(expr "$VARIABLES_DONT_EXPORT_AMOUNT" - 1))
-do
-  VARIABLE=$(plutil -extract "variablesdontexport.$i" raw -o - ./info.plist)
-  plutil -replace "variables.$VARIABLE" -string "" ./info.plist
-
-  echo " * $VARIABLE: cleared"
-done
-echo
-
 mkdir releases 2> /dev/null
 zip "releases/$FILENAME" -r dist img *.png info.plist
 echo
@@ -51,6 +32,5 @@ echo "Released $NAME v$VERSION"
 echo " * releases/$FILENAME"
 echo
 
-# restore original info.plist
-rm info.plist
-mv old.plist info.plist
+echo "Opening new release"
+open "./releases/$FILENAME"
